10037970eb9baa3dbd77f7d75aedf4d286125bb3
Arseniy <ashkaptsov@gmail.com>
2021-01-02 17:24:27 +0100

    module17

---
+++ module17new/config.php
@@ -0,0 +1,8 @@
+<?php
+
+
+define('URL', '/module17new'); // URL текущей страницы
+define('UPLOAD_MAX_SIZE', 6000000); // 1mb
+define('ALLOWED_TYPES', ['image/jpeg', 'image/png', 'image/gif']);
+define('UPLOAD_DIR', 'uploads');
+define('COMMENT_DIR', 'comments');
